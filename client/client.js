e7d0c2edff407dd9cc4e1f947546b1026b495d17
Bendinh <bendinh0805@gmail.com>
2023-04-07 16:25:01 -0400

    Final

--- client/client.js
+++ client/client.js
@@ -1,6 +1,67 @@
 const form = document.querySelector("form")
+const loadingElement = document.querySelector('.loading');
+const tweetsElements = document.querySelector(".tweets");
+const API_URL = "http://localhost:8000/tweet";
+
+const listAllTweets = () => {
+    tweetsElements.innerHTML = '';
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((allTweets) => {
+            console.log(allTweets);
+            allTweets.reverse();
+            allTweets.forEach((tweet) => {
+                const div = document.createElement("div");
+
+                const header = document.createElement("h3");
+                header.textContent = tweet.name;
+
+                const content = document.createElement("p");
+                content.textContent = tweet.content;
+
+                const date = document.createElement("small");
+                date.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(content);
+                div.appendChild(date);
+
+                tweetsElements.appendChild(div); ///////////////////////////////////////////////////////////
+            });
+        });
+}
+
+loadingElement.style.display = 'none';
+
+listAllTweets();
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Hello World!");
-})
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const tweet = {
+        name,
+        content
+    };
+    console.log(tweet);
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+
+    fetch(API_URL, {
+        method:"POST",
+        body: JSON.stringify(tweet),
+        headers:{
+            'content-type':'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(createdTweet => {
+            console.log(createdTweet)
+            form.reset();
+            form.style.display = '';
+            loadingElement.style.display = 'none';
+            listAllTweets();
+        });
+});
---
+++ server/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var cors = require('cors');
+var morgan = require('morgan');
+var monk = require('monk');
+var Filter = require('bad-words');
+
+var app = express();
+
+var db = monk('127.0.0.1/twiiter');
+filter = new Filter();
+
+var tweets = db.get('tweets')
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+const isValidTweet = (tweet) => {
+    if (tweet.name && tweet.name.toString().trim() !== '' && tweet.content && tweet.content.toString().trim() !== '' ){
+        return true
+    } else {
+        return false
+    }
+}
+
+app.get('/', (req, res) => {
+    res.json({
+        message:"Hi"
+    })
+});
+
+app.get("/tweet", (req,res) => {
+    tweets.find().then((allTweets) => {
+        res.json(allTweets);
+    })
+})
+
+app.post('/tweet', (req,res) => {
+    if (isValidTweet(req.body)){
+        const tweet = {
+            name: filter.clean(req.body.name.toString()),
+            content: filter.clean(req.body.content.toString()),
+            created: new Date()
+        }
+        tweets.insert(tweet).then((createdTweet) => {
+            res.json(createdTweet);
+        })
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Name and Content are required'
+        })
+    }
+})
+
+app.listen(8000, () => {
+    console.log('Listening on port 8000 ...')
+});
